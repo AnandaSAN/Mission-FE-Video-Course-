d9b9906be7b3f87be4b97c31c70ede6232c725e4
AnandaSAN <cacoasan1@gmail.com>
2026-02-09 23:42:26 +0700

    Update and add card to homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -180,9 +180,154 @@ const Home = () => {
             </button>
           </div>
         </div>
+
+        <div className="mt-6 md:mt-16">
+          <div>
+            <h1 className="font-poppins font-semibold text-2xl text-[#222325]">
+              Koleksi Video Pembelajaran Unggulan
+            </h1>
+            <p className="mt-2.5 font-dm-sans font-medium text-sm text-[#333333AD]">
+              Jelajahi Dunia Pengetahuan Melalui Pilihan Kami!
+            </p>
+          </div>
+          <div className="mt-6 md:mt-8">
+            <ul className="flex flex-nowrap whitespace-nowrap gap-1.5 overflow-x-auto hide-scrollbar">
+              <li className="relative py-3 pr-9 text-sm font-dm-sans font-medium text-[#f64920]  after:content-[''] after:absolute after:left-0 after:rounded-[10px] after:w-13 after:border-b-[6px] after:border-[#f64920] after:bottom-0">
+                <a href="#">Semua Kelas</a>
+              </li>
+              <li className="py-3 pr-9 text-sm font-medium text-gray-800 hover:text-[#f64920]">
+                <a href="#">Pemasaran</a>
+              </li>
+              <li className="py-3 pr-9 text-sm font-medium text-gray-800 hover:text-[#f64920]">
+                <a href="#">Design</a>
+              </li>
+              <li className="py-3 pr-9 text-sm font-medium text-gray-800 hover:text-[#f64920]">
+                <a href="#">Pengembangan Diri</a>
+              </li>
+              <li className="py-3 pr-9 text-sm font-medium text-gray-800 hover:text-[#f64920]">
+                <a href="#">Bisnis</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="mt-6 md:mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 md:gap-6 md:space-y-2">
+          {content.map((item) => (
+            <div
+              key={item.id}
+              className="flex flex-col bg-white border border-gray-200 rounded-xl p-4 w-full max-w-112.5 mx-auto shadow-md transition hover:shadow-lg"
+            >
+              <div className="flex sm:hidden gap-3">
+                <img
+                  src={item.img}
+                  alt="Content"
+                  className="w-25 h-25 rounded-lg object-cover shrink-0"
+                />
+                <div className="flex flex-col justify-center gap-2">
+                  <p className="font-poppins font-semibold text-[16px] text-[#222325]">
+                    Big 4 Auditor Financial Analyst
+                  </p>
+                  <div className="flex items-center gap-2">
+                    <img
+                      src={item.profil}
+                      alt="profile"
+                      className="w-9 h-9 rounded-[10px] object-cover"
+                    />
+                    <div className="flex flex-col">
+                      <span className="font-dm-sans font-medium text-sm text-[#222325]">
+                        Jenna Ortega
+                      </span>
+                      <p className="font-dm-sans font-normal text-[12px] text-[#333333AD]">
+                        Senior Accountant
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="hidden sm:flex flex-col gap-4">
+                <img
+                  src={item.img}
+                  alt="content"
+                  className="w-full h-45 object-cover rounded-[10px]"
+                />
+                <div className="flex flex-col gap-2">
+                  <p className="font-poppins font-semibold text-[18px] text-[#222325]">
+                    Big 4 Auditor Financial Analyst
+                  </p>
+                  <p className="font-dm-sans font-medium text-[16px] text-[#333333AD]">
+                    Mulai transformasi dengan instruktur profesional, harga yang
+                    terjangkau, dan ...
+                  </p>
+                </div>
+                <div className="flex items-center gap-2.5">
+                  <img
+                    src={item.profil}
+                    alt="profile"
+                    className="w-10 h-10 rounded-[10px]"
+                  />
+                  <div>
+                    <p className="font-dm-sans font-medium text-[16px] text-[#222325]">
+                      Jenna Ortega
+                    </p>
+                    <p className="font-dm-sans font-normal text-sm text-[#333333AD]">
+                      Senior Accountant di{" "}
+                      <span className="font-bold">Gojek</span>
+                    </p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="flex justify-between items-center mt-2">
+                <div className="flex items-center gap-2">
+                  <img src={Rating} alt="rating" className="w-20 md:w-22.5" />
+                  <p className="text-xs md:text-sm text-gray-500 underline">
+                    3.5 (86)
+                  </p>
+                </div>
+                <div className="text-base md:text-lg font-bold text-green-500">
+                  Rp 300K
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <div className="relative w-full max-w-7xl overflow-hidden rounded-[10px] text-white shadow-lg mx-auto px-5 py-12 my-10 md:px-15 lg:px-30 xl:px-85 lg:py-23">
+          <img
+            src={NewsLetter}
+            alt="newsbege"
+            className="absolute  object-cover w-full h-full left-0 top-0 brightness-[0.2] z-0"
+          />
+          <div className="relative z-10 flex flex-col items-center text-center gap-1">
+            <p className="font-dm-sans font-medium text-[16px] text-[#C1C2C4] tracking-wide">
+              NEWSLETTER
+            </p>
+            <h1 className="font-poppins font-semibold text-2xl">
+              Mau Belajar Lebih Banyak?
+            </h1>
+            <p className="mt-1.5 font-dm-sans font-normal text-sm text-[#F4F5FA] tracking-wide">
+              Daftarkan dirimu untuk mendapatkan informasi terbaru dan penawaran
+              spesial dari program-program terbaik harisenin.com
+            </p>
+
+            <div className="w-full max-w-md flex flex-col gap-4 overflow-hidden mt-8 md:relative">
+              <input
+                type="text"
+                placeholder="Masukkan Emailmu"
+                className="pr-2 pl-3 py-2.5 text-center bg-white text-[#333333AD] font-dm-sans font-normal text-sm rounded-[10px] focus:outline-none md:text-left md:pr-35 xl:pr-34 xl:h-10"
+              />
+              <button className="bg-[#FFBD3A] px-19.5 py-2 rounded-[10px] transition text-[16px] font-dm-sans font-bold hover:bg-[#ffb835] md:absolute md:px-5 md:py-1 md:right-2 md:bottom-1/2 md:translate-y-1/2">
+                Subscribe
+              </button>
+            </div>
+          </div>
+        </div>
       </main>
 
-      <footer className="bg-[#fdf8f2] px-6 py-4 md:px-30 md:py-16"></footer>
+      <footer className="bg-[#fdf8f2] px-6 py-4 md:px-30 md:py-16">
+        
+      </footer>
     </div>
   );
 };
